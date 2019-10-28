351bef9632a9ecb4be06877c168ebe180dba044f
xiaodi <823497477@qq.com>
2019-10-28 13:12:05 +0800

    1

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -7,10 +7,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+//开启Hystrix熔断机制
 @EnableCircuitBreaker
+//声明服务提供方
 @EnableDiscoveryClient
 @SpringBootApplication
 public class App {
+	//注入调用微服务类
 	@Bean
 	@LoadBalanced
 	RestTemplate restTemplate(){
--- src/main/java/test/HelloServiceImpl.java
+++ src/main/java/test/HelloServiceImpl.java
@@ -11,9 +11,18 @@ public class HelloServiceImpl implements HelloService{
 	
 	@Autowired
 	RestTemplate res;
-	
+	//设置容错回调函数
 	@HystrixCommand(fallbackMethod="helloFallBack")
 	public String helloService() {
+//		get请求
+//		responseEntity<String> re = rt.getForEntity("http://xxx/user?name={1}"(或者?name={name}),String.class(返回类型),"参数"(或者Map));
+//		String a = re.getBody();
+//
+//		String a = rt.getForObject(uri,string.class);
+//
+//		post请求（同上）
+//		postForEntity
+//		postForObject
 		return res.getForObject("http://HELLO-SERVICE/hello", String.class);
 	}
 	
